d134a8e32b240bf31d89d6db943320b0ab4ba6f4
V-2Schneider <jodieczeci@gmail.com>
2019-11-11 17:17:12 +0100

    Translated names of variables and updated names of loaded images

--- modules/draw.cpp
+++ modules/draw.cpp
@@ -3,38 +3,38 @@
 #include <allegro5/allegro_font.h>
 #include <enemy.h>
 
-ALLEGRO_BITMAP *statek, *tlo, *pocisk, *tarcza, *tarczaX, *enemy_missle, *dead_ship, *ayyylien, *ayyylienX, *game_over, *statek_un;
-ALLEGRO_BITMAP *menu_back, *new_game_g, *load_game_g, *exit_g, *border, *mission_end, *end_screen, *tlo2, *tlo3, *high_scores_g, *high_scores_back;
-ALLEGRO_BITMAP *yyh, *yyhX;
+ALLEGRO_BITMAP *ship, *background, *bullet, *target, *targetX, *enemy_missle, *dead_ship, *ayyylien, *ayyylienX, *game_over, *ship_un;
+ALLEGRO_BITMAP *menu_back, *new_game_g, *load_game_g, *exit_g, *border, *mission_end, *end_screen, *background2, *background3, *high_scores_g, *high_scores_back;
+ALLEGRO_BITMAP *ev_spaceship, *ev_spaceshipX;
 ALLEGRO_FONT *font8;
 int back_y[2]={-600, -1800};
 
 void load_graph()
 {
-    statek = al_load_bitmap("graph/statek/podst.png");
-    statek_un = al_load_bitmap("graph/statek/untouchable.png");
-    dead_ship = al_load_bitmap("graph/statek/wybuch.png");
-    tlo = al_load_bitmap("graph/tlo/Testowanie3.png");
-    tlo2 = al_load_bitmap("graph/tlo/Testowanie4.png");
-    tlo3 = al_load_bitmap("graph/tlo/Testowanie5.png");
-    pocisk = al_load_bitmap("graph/statek/pocisk.png");
-    tarcza = al_load_bitmap("graph/przeciwnicy/tarcza.png");
-    tarczaX = al_load_bitmap("graph/przeciwnicy/tarcza-X.png");
-    ayyylien = al_load_bitmap("graph/przeciwnicy/kosmita.png");
-    ayyylienX = al_load_bitmap("graph/przeciwnicy/kosmita-X.png");
-    yyh = al_load_bitmap("graph/przeciwnicy/yhhhhh.png");
-    yyhX = al_load_bitmap("graph/przeciwnicy/yhhhhhX.png");
-    enemy_missle = al_load_bitmap("graph/przeciwnicy/amunicja/pocisk.png");
+    ship = al_load_bitmap("graph/ship/normal.png");
+    ship_un = al_load_bitmap("graph/ship/untouchable.png");
+    dead_ship = al_load_bitmap("graph/ship/explosion.png");
+    background = al_load_bitmap("graph/background/Testing3.png");
+    background2 = al_load_bitmap("graph/background/Testing4.png");
+    background3 = al_load_bitmap("graph/background/Testing5.png");
+    bullet = al_load_bitmap("graph/ship/bullet.png");
+    target = al_load_bitmap("graph/enemies/target.png");
+    targetX = al_load_bitmap("graph/enemies/target-X.png");
+    ayyylien = al_load_bitmap("graph/enemies/alien.png");
+    ayyylienX = al_load_bitmap("graph/enemies/alien-X.png");
+    ev_spaceship = al_load_bitmap("graph/enemies/ev_spaceship.png");
+    ev_spaceshipX = al_load_bitmap("graph/enemies/ev_spaceshipX.png");
+    enemy_missle = al_load_bitmap("graph/enemies/ammo/bullet.png");
     game_over = al_load_bitmap("graph/menu/game_over.png");
-    menu_back = al_load_bitmap("graph/menu/tlo.png");
-    new_game_g = al_load_bitmap("graph/menu/nowa_gra.png");
-    load_game_g = al_load_bitmap("graph/menu/wczytaj_gre.png");
-    high_scores_g = al_load_bitmap("graph/menu/najlepsze_wyniki.png");
-    high_scores_back = al_load_bitmap("graph/menu/wyniki.png");
-    exit_g = al_load_bitmap("graph/menu/wyjdz.png");
-    border = al_load_bitmap("graph/menu/ramka2.png");
-    mission_end = al_load_bitmap("graph/menu/misja_zakonczona.png");
-    end_screen = al_load_bitmap("graph/menu/koniec_gry.png");
+    menu_back = al_load_bitmap("graph/menu/background.png");
+    new_game_g = al_load_bitmap("graph/menu/new_game.png");
+    load_game_g = al_load_bitmap("graph/menu/load_game.png");
+    high_scores_g = al_load_bitmap("graph/menu/highscores.png");
+    high_scores_back = al_load_bitmap("graph/menu/scores.png");
+    exit_g = al_load_bitmap("graph/menu/exit.png");
+    border = al_load_bitmap("graph/menu/frame2.png");
+    mission_end = al_load_bitmap("graph/menu/mission_end.png");
+    end_screen = al_load_bitmap("graph/menu/the_end.png");
     font8 = al_create_builtin_font();
 }
 
@@ -70,7 +70,7 @@ void draw_level(ALLEGRO_BITMAP *enemy_1, ALLEGRO_BITMAP *enemy_1X, ALLEGRO_BITMA
         al_draw_bitmap (background,0,back_y[0],0);
         al_draw_bitmap (background,0,back_y[1],0);
         for (int i = 0 ; i<3 ; i++)
-            if (projectile[i] == true) al_draw_bitmap (pocisk, shot_x[i], shot_y[i], 0);
+            if (projectile[i] == true) al_draw_bitmap (bullet, shot_x[i], shot_y[i], 0);
         if (patterns[0] == true)
         {
             draw_enemies(enemy_1, enemy_1X, how_many);
@@ -87,9 +87,9 @@ void draw_level(ALLEGRO_BITMAP *enemy_1, ALLEGRO_BITMAP *enemy_1X, ALLEGRO_BITMA
             draw_missles(enemy_missle, how_many);
         }
         if ((player_dead == false)&&(untouchable == false))
-            al_draw_bitmap (statek,player_x,player_y,0);
+            al_draw_bitmap (ship,player_x,player_y,0);
         if (untouchable == true)
-            al_draw_bitmap (statek_un,player_x,player_y,0);
+            al_draw_bitmap (ship_un,player_x,player_y,0);
         al_draw_textf(font8,al_map_rgb(255,255,0), 10, 10, 0,"LIVES: %d, x=%3d , y=%3d", lives, player_x, player_y);
         al_draw_textf(font8,al_map_rgb(255,255,0), 700, 10, 0,"SCORE=%6d", score);
 }
